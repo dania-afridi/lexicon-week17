ae9c5910489f8eab7626a6d0ab0c166038cd3c3d
dania-afridi <dania.awais33@gmail.com>
2024-04-25 18:49:52 +0200

    calculator folder added.

---
+++ calculator/simpleCalculator.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Calculator
+{
+    public class program{
+        static void Main(string[] args) { 
+            Console.WriteLine("--------------------Calculator---------------------"); 
+        }
+    }
+}
+
